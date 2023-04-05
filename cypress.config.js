292bc0be8fa8e6b5e677626eaa19b3f1704ffd08
Arthur <arthur@MacBook-Pro-M1-Clayton.local>
2023-04-05 13:02:12 -0300

    case invalid email

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://www.magazineluiza.com.br/',
+    viewportWidth: 1440,
+    viewportHeight: 900
   },
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,27 @@
-it('login successfully', function(){
-    cy.visit('https://www.magazineluiza.com.br/')
+//it('login successfully', function(){
+//    cy.visit('https://www.magazineluiza.com.br/')
+//    cy.get('a[class="sc-AHaJN doAIBk sc-nTrUm hrivJS sc-nTrUm hrivJS"]').click()
+
+//    cy.get('input[class=FormGroup-input]', {timeout: 5000}).eq(1).type('art@mailinator.com')
+//    cy.get('input[class=FormGroup-input]', {timeout: 5000}).eq(2).type('Teste@123')
+
+//    cy.get('button[class="LoginBox-form-continue"]').eq(1).click()
+
+//    const expectedText =  'Olá, '
+//    cy.get('button[class="sc-AHaJN jhvpMG sc-jfTVlA lmNEil sc-jfTVlA lmNEil"]')
+//        .should('be.visible', expectedText)
+//})
+
+it('login email invalid', function(){
+    cy.visit('/')
     cy.get('a[class="sc-AHaJN doAIBk sc-nTrUm hrivJS sc-nTrUm hrivJS"]').click()
 
-    cy.get('input[class=FormGroup-input]', {timeout: 5000}).eq(1).type('art@mailinator.com')
+    cy.get('input[class=FormGroup-input]', {timeout: 5000}).eq(1).type('artkjhej@mailinator.com')
     cy.get('input[class=FormGroup-input]', {timeout: 5000}).eq(2).type('Teste@123')
 
     cy.get('button[class="LoginBox-form-continue"]').eq(1).click()
 
-    const expectedText =  'Olá, '
-    cy.get('button[class="sc-AHaJN jhvpMG sc-jfTVlA lmNEil sc-jfTVlA lmNEil"]')
+    const expectedText = 'Verifique o login e a senha para continuar.'
+    cy.get('div[class="LoginBox-form-error--unique"]')
         .should('be.visible', expectedText)
 })
